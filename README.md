3d8f51dc8e7159b184a21c2580a67fb0088877fb
Harry Xue <harry.xue@hotmail.com>
2015-10-01 22:19:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# bus-tracker-leaflet
+> This temporary repository will be the website frontend of the bus tracker. Everything will eventually be hosted by the school. 
+
+### Info
+* This package runs on [Node.js][1]. 
+* Even though Node.js does not support a browser, the school will provide AWS for us to migrate our codes into. 
+
+----
+### Usage
+1. The package runs on [Node.js][1]. If you do not have it installed on your computer, please download [here][1].
+2. In the command line, type
+	* `npm install`
+	* `npm start`
+3. Then, go to `http://127.0.0.1:8080` to see the webpage, which will eventually be on the school website (To be implemented).
+	
+[1]:https://nodejs.org
+
+----
+Copyright (c) 2015 MXProgrammingClub and its members
+Licensed under the BSD 3 Clause
---
+++ site/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var mime = require('mime');
+
+function send404 (response) {
+	response.writeHead(404, {"Content-type" : "text/plain"});
+	response.write("Error 404: Resource not found");
+	response.end();
+}
+
+function sendPage (response, filePath, fileContents) {
+	response.writeHead(200, { "Content-type": mime.lookup(path.basename(filePath)) });
+	response.end(fileContents);
+}
+
+function serverWorking (response, absPath) {
+	fs.exists(absPath, function(exists) {
+		if (exists) {
+			fs.readFile(absPath, function (e, data) {
+				if (e) send404(response);
+				else sendPage(response, absPath, data);
+			});
+		} else send404(response);
+	});
+}
+
+var server = http.createServer(function (request, response) {
+	var filePath;
+	if (request.url === '/') filePath = 'index.html';
+	else filePath = request.url;
+	
+	serverWorking(response, "./" + filePath);
+});
+
+server.listen(process.env.PORT || 8080);
+console.log('Please go to http://127.0.0.1:8080 to test');
