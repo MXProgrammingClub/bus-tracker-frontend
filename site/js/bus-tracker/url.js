97df8a71dafe216e3e3b98f16f601558073f1984
Harry Xue <harry.xue@hotmail.com>
2015-10-25 21:21:05 -0400

    Reworked server URL

--- site/js/bus-tracker/url.js
+++ site/js/bus-tracker/url.js
@@ -1,7 +1,7 @@
 "use strict";
 
 define(function () {
-	var server = 'http://127.0.0.1:8789';
+	var server = window.location.href.substring(0, window.location.href.indexOf(':', 6)) + ':8789';
 	var token = 'pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ';
 	var titleLayer = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + token;
 	var attribution = '<a href="https://www.mapbox.com/about/maps/">© Mapbox © OpenStreetMap</a>';
