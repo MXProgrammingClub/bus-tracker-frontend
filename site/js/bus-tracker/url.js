1c6827dfc70b93f9ebdb25c5bb5db107fea0706e
Harry Xue <harry.xue@hotmail.com>
2015-11-06 15:36:12 -0500

    Updated server request url

--- site/js/bus-tracker/url.js
+++ site/js/bus-tracker/url.js
@@ -1,7 +1,7 @@
 "use strict";
 
 define(function () {
-	var server = 'http://10.3.108.10:8789/zUpdate';
+	var server = 'http://10.3.108.10:8789/zebra';
 	var token = 'cGsuZXlKMUlqb2lhR0Z5Y25rdGVIVmxNVGs1T1NJc0ltRWlPaUpqYVdacloyVXdjblZrYkdRMmN6UnROMncwZUdWeGVtRm1JbjAuN3Z2RGFwMzZoajNuTHI2bS1kT0pPQQ=='
 	var layer = 'aHR0cHM6Ly9hLnRpbGVzLm1hcGJveC5jb20vdjQve2lkfS97en0ve3h9L3t5fS5wbmc/YWNjZXNzX3Rva2VuPQ==';
 	var attribution = 'PGEgaHJlZj0iaHR0cHM6Ly93d3cubWFwYm94LmNvbS9hYm91dC9tYXBzLyI+wqkgTWFwYm94IMKpIE9wZW5TdHJlZXRNYXA8L2E+';
