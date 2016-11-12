216917e9cb1477ee32e6e4b7c6cd409ea3d4a1c3
Harry Xue <harry.xue@hotmail.com>
2016-11-12 11:53:56 -0500

    Completed tab toggle functions

---
+++ site/js/tab-contents/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+define(['minify!./map.html', 'minify!./schedule.html', 'minify!./about.html'], function (map, schedule, about) {
+    return [map, schedule, about].join();
+});
--- site/js/ui.js
+++ site/js/ui.js
@@ -1,5 +1,31 @@
 'use strict';
 
-define(['jquery', 'minify!../tab-contents/map.html'], function ($, mapHTML) {
-    $(mapHTML).appendTo('div.tab-contents');
+define(['jquery', 'tab-contents/main'], function ($, tabHTML) {
+    var $container = $('div.tab-contents');
+    $(tabHTML).appendTo($container);
+
+    // Toggle actions
+    $('.toggle').on('click', function (event) {
+        event.preventDefault();
+
+		var c = 'active';
+
+        // The tab that is currently active
+        var $this = $('.tab-contents-toggle').find('.' + c).children();
+
+        // The tab that the user clicks on
+        var $that = $('.tab-contents-toggle').find(`.toggle[toggle="${$(this).attr('toggle')}"]`); 
+
+        // Current active content
+        var $current = $container.find($this.attr('toggle'));
+
+        // Target active content
+        var $target = $container.find($that.attr('toggle'));
+
+        // Activates proper tabs
+		$current.removeClass(c);
+		$target.addClass(c);
+		$this.parent().removeClass(c);
+		$that.parent().addClass(c);
+    });
 });
