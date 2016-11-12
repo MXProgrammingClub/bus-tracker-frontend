4f260c711ea759c04bb5dc80759ecfb17b1c8a91
Harry Xue <harry.xue@hotmail.com>
2016-11-12 13:15:29 -0500

    Bug fix for text module

--- site/js/schedule.js
+++ site/js/schedule.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
-define(["jquery", "minify!tab-contents/scheduleTimes.html"], function($, scheduleTimes) {
+define(['jquery', 'text!tab-contents/scheduleTimes.html'], function($, scheduleTimes) {
 	$(document).ready(function() {
 		var date = new Date();
 		var dayOfTheWeek = date.getDay();
 		if(dayOfTheWeek === 0) {
-    		$("#schedule-box").html($(scheduleTimes).filter("#sundaySchedule"));
+    		$('#schedule-box').html($(scheduleTimes).filter('#sundaySchedule'));
 		}
 		else if(dayOfTheWeek >= 1 && dayOfTheWeek <= 5) {
-    		$("#schedule-box").html($(scheduleTimes).filter("#weekdaySchedule"));
+    		$('#schedule-box').html($(scheduleTimes).filter('#weekdaySchedule'));
 		}
 		else if(dayOfTheWeek === 6) {
-    		$("#schedule-box").html($(scheduleTimes).filter("#saturdaySchedule"));
+    		$('#schedule-box').html($(scheduleTimes).filter('#saturdaySchedule'));
 		}
 	});
 });
